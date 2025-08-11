9019ffb7f9af43e73dde22fa5a3aa7c4c55c264c
Mark Moxon <mark@moxon.net>
2025-08-11 15:31:39 +0100

    Move music variables outside of music code so values are not overwritten, move music files into directory M

--- elk-elite-music-huffman.asm
+++ elk-elite-music-huffman.asm
@@ -14,15 +14,35 @@ SHEILA_MISC_CONTROL     = $FE07
 ;ORG &3000
 ;GUARD &6000
 
-ORG &0000  ; Use this for Electron Elite
+ ORG &0000   ; Use this for Electron Elite
 ;ORG &70     ; USE FOR BASIC player.bas ONLY!
 
 .zp_start
 .huffmunch_zpblock  SKIP 4      ; Share with four-byte variable RAND at &0000
 .zp_end
 
+ ORG &1C00
+
+.musicStatus
+
+ SKIP 1                 \ A flag to determine whether to play the currently
+                        \ selected music:
+                        \
+                        \   * 0 = do not play the music
+                        \
+                        \   * &FF = do play the music
+
+.huffmunch_block    SKIP 5
+.page_bytes         SKIP 2
+.byte_ptr           SKIP 2
+.track_start        SKIP 2
+.xsav               SKIP 1
+.ysav               SKIP 1
+.zp_temp            SKIP 4
+
+
 ORG &0E00       ; MM - assemble music and driver at start of
-GUARD &1D00     ;      available memory in Electron
+GUARD &1C00     ;      available memory in Electron
 
 .start
 
@@ -35,6 +55,9 @@ GUARD &1D00     ;      available memory in Electron
     jmp poll_track      ; &0E03
     jmp LoadMusic1      ; &0E06
     jmp LoadMusic2      ; &0E09
+    jmp StopMusic       ; &0E0C
+    jmp StartMusic      ; &0E0F
+    jmp LoadPlayMusic1  ; &0E12
 
 .init_tune1
     jsr swap_zp             ; MM - preserve zero page &0000 to &0004
@@ -79,33 +102,6 @@ GUARD &1D00     ;      available memory in Electron
     LDY ysav
     RTS
 
-; MM - move non-zp variables here
-
-.huffmunch_block    SKIP 5
-.page_bytes         SKIP 2
-.byte_ptr           SKIP 2
-.track_start        SKIP 2
-.xsav               SKIP 1
-.ysav               SKIP 1
-
-                        \ --- Mod: Code added for music: ---------------------->
-
-.musicStatus
-
- EQUB 0                 \ A flag to determine whether to play the currently
-                        \ selected music:
-                        \
-                        \   * 0 = do not play the music
-                        \
-                        \   * &FF = do play the music
-
-                        \ --- End of added code ------------------------------->
-
-
-; MM - Add storage for original contents of RAND
-
-.zp_temp            SKIP 4
-
 .swap_zp
 
  LDA &0000              \ Swap RAND (&0000-&0003) with zp_temp
@@ -128,33 +124,63 @@ GUARD &1D00     ;      available memory in Electron
  STA zp_temp+3
  STX &0003
 
- RTS
-
-.MUSIC1
-
- EQUS "L.MUSIC1"        \ This is short for "*LOAD MUSIC1"
- EQUB 13
-
-.MUSIC2
-
- EQUS "L.MUSIC2"        \ This is short for "*LOAD MUSIC2"
- EQUB 13
+ RTS                    \ Return from the subroutrine
 
 .LoadMusic1
+
+ JSR StopMusic          \ Stop any music from playing
 
  LDX #LO(MUSIC1)        \ Set (Y X) to point to the OS command at MUSIC1, which
  LDY #HI(MUSIC1)        \ loads the Elite Theme music file
 
  JMP OSCLI              \ Call OSCLI to execute the OS command at (Y X), which
-                        \ loads the Elite Theme music file
+                        \ loads the Elite Theme music file, and return from the
+                        \ subroutine using a tail call
+
+.MUSIC1
+
+ EQUS "L.M.MUSIC1"      \ This is short for "*LOAD M.MUSIC1"
+ EQUB 13
 
 .LoadMusic2
 
- LDX #LO(MUSIC2)        \ Set (Y X) to point to the OS command at MUSIC1, which
- LDY #HI(MUSIC2)        \ loads the Elite Theme music file
+ JSR StopMusic          \ Stop any music from playing
+
+ LDX #LO(MUSIC2)        \ Set (Y X) to point to the OS command at MUSIC2, which
+ LDY #HI(MUSIC2)        \ loads the Blue Danube music file
 
  JMP OSCLI              \ Call OSCLI to execute the OS command at (Y X), which
-                        \ loads the Elite Theme music file
+                        \ loads the Blue Danube music file, and return from the
+                        \ subroutine using a tail call
+
+.MUSIC2
+
+ EQUS "L.M.MUSIC2"      \ This is short for "*LOAD M.MUSIC2"
+ EQUB 13
+
+.StopMusic
+
+ LDA #0                 \ Stop any music from playing
+ STA musicStatus
+
+ RTS                    \ Return from the subroutrine
+
+.StartMusic
+
+ LDA #&FF               \ Stop any music from playing
+ STA musicStatus
+
+ RTS                    \ Return from the subroutrine
+
+.LoadPlayMusic1
+
+ JSR LoadMusic1         \ Load the Elite Theme music file
+
+ JSR init_tune1         \ Initialise the music
+
+ JSR StartMusic         \ Start the music playing
+
+ RTS                    \ Return from the subroutrine
 
 INCLUDE "lib/huffman.s.asm"
 INCLUDE "drivers/huffman.asm"
@@ -182,11 +208,13 @@ PRINT "      Tune 1 size is ",P%-tune_data1_start,"bytes"
 ;
 ;PRINT "      Tune 2 size is ",P%-tune_data2_start,"bytes"
 
-H%=P%
-ALIGN 256
-PRINT "           Alignment lost ",(P%-H%),"bytes"
+;H%=P%
+;ALIGN 256
+;PRINT "           Alignment lost ",(P%-H%),"bytes"
 
 .end
+
+; MM - move non-zp variables here, so they do not get overwritten by music code
 
 PRINT "           Total size is ",(end-start),"bytes"
 PRINT ""
